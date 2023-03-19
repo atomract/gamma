9d2c64e5178ba33578c61893567819682b0c8874
Visinigiri Aditya <adityakumarverified@gmail.com>
2023-03-19 23:35:29 +0530

    First Page

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./styles.css";
 import { Canvas, useLoader } from "@react-three/fiber";
 import { Html, OrbitControls, RenderTexture, Text } from "@react-three/drei";
@@ -10,25 +10,30 @@ import {
 } from "three";
 import image from "../assets/c.png";
 import Earth from "./Earth";
+import ReactPlayer from "react-player";
+import Logo from "../assets/logo.png";
+
+import BGVideo from "../assets/video.mp4";
 
 const Main = () => {
   const [ImageTexture] = useLoader(TextureLoader, [image]);
 
   let width = window.innerWidth;
   let height = window.innerHeight;
+  const videoRef = useRef();
   return (
-    <div className="container">
-      {/* <HomePageText /> */}
-      <Canvas>
-        {/* PerspectiveCamera( 45, width / height, 1, 1000 ); */}
-        <OrbitControls
+    // <div className="container">
+    //   {/* <HomePageText /> */}
+    //   <Canvas>
+    // {/* PerspectiveCamera( 45, width / height, 1, 1000 ); */}
+    /* <OrbitControls
           enableZoom={false}
           enablePan={false}
           enableRotate={false}
         />
-        <ambientLight intensity={1} />
-        {/* <directionalLight position={[1, 1, 20]} intensity={1.5} /> */}
-        <mesh>
+        <ambientLight intensity={1} /> */
+    /* <directionalLight position={[1, 1, 20]} intensity={1.5} /> */
+    /* <mesh>
           <sphereGeometry args={[50, 320, 320]} />
           <meshStandardMaterial side={DoubleSide} map={ImageTexture} />
           <perspectiveCamera
@@ -36,15 +41,60 @@ const Main = () => {
             makeDefault={true}
             position={[45, width / height, 1, 1000]}
           />
-        </mesh>
+        </mesh> */
+    /* 
         <mesh>
-          <Html>
-            <div className="maintext">ULTIMATE LIFE</div>
+          <Html as="div" className="">
+            <div className="d-flex" style={{ display: "flex" }}>
+              <div>
+                <ReactPlayer
+                  controls={true}
+                  style={{
+                    position: "absolute",
+                    height: "100vh",
+                    width: "100vh",
+                    top: "0",
+                    left: "0",
+                    transform: "none",
+                  }}
+                  url="https://www.youtube.com/watch?v=ysz5S6PUM-U"
+                />
+              </div>
+              <div>
+                <h1>Hello World</h1>
+              </div>
+            </div>
           </Html>
         </mesh>
-        <Earth />
-      </Canvas>
-    </div>
+        {/* <Earth /> */
+    //   </Canvas>
+    // </div> */}
+
+    <>
+      <div style={{ position: "fixed", top: 0, left: 0, zIndex: -1 }}>
+        <video
+          ref={videoRef}
+          src={BGVideo}
+          autoPlay
+          muted
+          loop
+          style={{ objectFit: "cover", width: "100%", height: "100%" }}
+        />
+      </div>
+      <div className="logoParent">
+        <img src={Logo} className="logo" width={500} height={350} alt="logo" />
+      </div>
+      <div style={{}}>
+        <h1 className="Maintext"> ULTIMATE LIFE </h1>
+        <h1 className="subText">MetaVerse</h1>
+      </div>
+      <div className="CanvaStyle">
+        <Canvas>
+          <directionalLight position={[5, 1, 20]} intensity={1.5} />
+          <Earth />
+        </Canvas>
+      </div>
+    </>
   );
 };
 
