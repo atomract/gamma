cf1bf93dd9a10b030981d158b0a3d881a0328d1e
Visinigiri Aditya <adityakumarverified@gmail.com>
2023-03-24 22:30:54 +0530

    thirdPageStarted

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,21 +1,21 @@
 import React from "react";
-import '../App.css'
-import logo from '../assets/logo.png'
+import "../App.css";
+import logo from "../assets/Gamma_Logo.svg";
+import HamburgerIcon from "../assets/bars-solid.svg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 function Navbar() {
   return (
-    <nav>
-      <div className="navbar-brand">
-        <img src={logo} alt="Logo" />
-        {/* <span className="bName">Gamma Cities</span> */}
+    <>
+      <div className="w-full flex items-center justify-between h-24 my-8 md:my-16 ">
+        <img src={logo} className="logo" width={500} height={350} alt="Logo" />
+        <FontAwesomeIcon
+          className="text-white text-3xl mr-4 md:mr-10"
+          icon={faBars}
+        />
       </div>
-      <ul className="navbar-menu">
-        {/* <li><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Services</a></li>
-        <li><a href="#">Contact</a></li> */}
-      </ul>
-    </nav>
+    </>
   );
 }
 
